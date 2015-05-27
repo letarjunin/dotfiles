df71a9b2939fe2caeee39ff8daee9cad19e3a882
Aj <letarjunin@gmail.com>
2015-05-27 00:12:58 -0500

    Adding missing common file

---
+++ common.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+all_files=".bashrc .bashrc.local .bash_aliases .bash_completion .bash_funcs .bash_profile .tmux.conf pb.rb pb_check.sh set_pb_wallpaper.rb  .vimrc.before.local .vimrc.local .tmuxinator/dead.yml"
+
+debug=true
+debug()
+{
+    if [ "$debug" = true ]; then
+        echo "Debug : $1"
+    fi
+}
+
