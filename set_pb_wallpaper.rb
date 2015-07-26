5a8e7b448a1b2e56dff46093fe00c1e7b16f3da9
AJ <letarjunin@gmail.com>
2015-07-25 20:45:12 -0500

    Adding pusbbullet websocket support. This negates the need to call set_pb_wallpaper.rb everytime I add a new image to pusbbullet.

--- set_pb_wallpaper.rb
+++ set_pb_wallpaper.rb
@@ -1,61 +1,6 @@
 #!/usr/bin/env ruby
 
-require 'washbullet'                                                     
-require 'open-uri'
-require 'shellwords'
-require 'pry'
-require_relative 'pb_private.rb'
+require_relative 'pb_wallpaper_api.rb'
 
-HOME = File.expand_path( "~" )
-TEMP_FILE = "#{ HOME }/Downloads/temp.jpeg"
-
-module OS_ENUM
-  LINUX = 0
-  MAC   = 1
-  WIN   = 2
-end
-
-def os_type
-  if RUBY_PLATFORM.include? ("linux")
-    return OS_ENUM::LINUX
-  elsif RUBY_PLATFORM.include? ("darwin")
-    return OS_ENUM::MAC
-  else
-    return OS_ENUM::WIN
-  end
-end
-
-def download_file( url, folder = TEMP_FILE )
-  File.open( TEMP_FILE, "wb") do |saved_file|
-    open( url, "rb" ) do |read_file|
-      saved_file.write(read_file.read)
-    end
-  end
-end
-
-def process_pushes
-    #Note, you need your own API_KEY defined here.
-    client = Washbullet::Client.new( API_KEY )
-    client.pushes.body[ "pushes" ].each do |p|
-      if p[ "active" ].to_s == 'true' and p[ 'type' ].to_s == 'file' and p[ 'file_type' ].to_s.include? ( 'image/jpeg' )
-        download_file( p[ 'file_url' ] )
-        return true
-      end
-    end
-    return false
-end
-
-def set_wallpaper
-  cmd = ""
-  if os_type == OS_ENUM::LINUX
-    cmd = "gsettings set org.gnome.desktop.background picture-uri file:///#{ TEMP_FILE }" 
-  elsif os_type == OS_ENUM::MAC
-    cmd = "osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"#{ TEMP_FILE }\"'; killall Dock"
-  else
-    return #WIN not supported yet!
-  end
-  exec cmd if process_pushes
-end
-
-set_wallpaper
+set_pb_wallpaper
 
