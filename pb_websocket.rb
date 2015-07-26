7bcc34187db0fecb95c7d2181a1a6751960fab7e
AJ <letarjunin@gmail.com>
2015-07-25 21:10:49 -0500

    Hack - After one run of the command, it errors out. For now a simple forced look should take care of this.

--- pb_websocket.rb
+++ pb_websocket.rb
@@ -10,31 +10,33 @@ require_relative 'pb_wallpaper_api.rb'
 
 WS_URL = 'https://stream.pushbullet.com/websocket/'
 
-DEBUG=true
+DEBUG=false
 
 def print_msg( msg )
   puts msg if DEBUG
 end
 
-EM.run{
-  time_now = Time.now.to_i
-  ws = Faye::WebSocket::Client.new( WS_URL + API_KEY )
+while(true)
+  EM.run{
+    time_now = Time.now.to_i
+    ws = Faye::WebSocket::Client.new( WS_URL + API_KEY )
 
-  ws.on :message do |event|
-    j = JSON.parse(event.data)
-    if( j["type"] == "nop" )
-      time_now = Time.now.to_i
-    end
-    if( j["type"] == "tickle" and j["subtype"] == "push" )
-      begin
-        set_wallpaper( time_now )
-      rescue Exception => e
-        print_msg "set_wallpaper function errored with -'#{ e.message }'"
+    ws.on :message do |event|
+      j = JSON.parse(event.data)
+      if( j["type"] == "nop" )
+        time_now = Time.now.to_i
+      end
+      if( j["type"] == "tickle" and j["subtype"] == "push" )
+        begin
+          set_wallpaper( time_now )
+        rescue Exception => e
+          print_msg "set_wallpaper function errored with -'#{ e.message }'"
+        end
       end
     end
-  end
 
-  ws.on :close do |event|
-    print_msg "Websocket is clsoing.! #{ event.reason }"
-  end
-}
+    ws.on :close do |event|
+      print_msg "Websocket is clsoing.! #{ event.reason }"
+    end
+  }
+end
