f7af5527c5cbab044b1f2a58b0b1d8144647fda6
Arjun <letarjunin@gmail.com>
2015-11-18 07:42:34 -0600

    Removing non-dotfiles from repo

--- reload_hid_driver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rmmod hid_multitouch 
-sleep 1
-modprobe hid_multitouch 
--- set_pb_wallpaper.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative 'pb_wallpaper_api.rb'
-
-set_wallpaper( SET_TYPE::WALLPAPER )
-
