b0b7147bd650668ba4237deee3c24c038c872e40
Arjun <arjunkar@amazon.com>
2016-06-24 07:48:50 -0700

    Adding custom bundles for vim, and adding .vimrc.bundles.local to install as part of the install script.

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-all_files=".bashrc .bashrc.local .bash_aliases .bash_completion .bash_funcs .bash_profile .tmux.conf pb.rb pb_check.sh set_pb_wallpaper.rb  .vimrc.before.local .vimrc.local .tmuxinator/dead.yml"
+all_files=".bashrc .bashrc.local .bash_aliases .bash_completion .bash_funcs .bash_profile .tmux.conf pb.rb pb_check.sh set_pb_wallpaper.rb  .vimrc.before.local .vimrc.local .tmuxinator/dead.yml .vimrc.bundles.local"
 
 debug=true
 debug()
