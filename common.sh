6747684b86bf28cc3b337014ab6557c5284db843
Arjun <arjunkar@amazon.com>
2017-02-18 11:30:02 -0800

    New  TMUX conf files form https://github.com/gpakosz/.tmux

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@
 app_dir="$HOME/.dot_vim"
 vim_files=".vimrc .vimrc.bundles .vimrc.before .vimrc.before.local .vimrc.bundles.local .vimrc.local"
 bash_files=".bashrc .bashrc.local .bash_aliases .bash_completion .bash_funcs .bash_profile"
-tmux_files=".tmux.conf .tmuxinator/dead.yml"
+tmux_files=".tmux.conf .tmux.conf.local .tmuxinator/dead.yml"
 all_files="$vim_files $bash_files $tmux_files"
 
 debug=true
