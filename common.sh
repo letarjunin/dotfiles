eafeb477cef70345e25ac9b11e0bfb350664e876
Arjun <arjunkar@amazon.com>
2018-03-10 07:37:06 -0800

    Allow for personal .zimrc and .zshrc

--- common.sh
+++ common.sh
@@ -2,7 +2,7 @@
 
 app_dir="$HOME/.dot_vim"
 vim_files=".vimrc .vimrc.plug.support .config/nvim/init.vim"
-bash_files=".bashrc .bashrc.local .bash_aliases .bash_completion .bash_funcs .bash_profile"
+bash_files=".bashrc .bashrc.local .bash_aliases .bash_completion .bash_funcs .bash_profile .zimrc_local .zshrc_local"
 tmux_files=".tmux.conf .tmux.conf.local"
 all_files="$vim_files $bash_files $tmux_files"
 
